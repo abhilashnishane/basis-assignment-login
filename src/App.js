e314bf8a242979ebb7d60de302c98ee23b6c3b27
Abhilash Nishane <abhilash.nishane6@gmail.com>
2020-11-19 12:40:21 +0530

    installled router, reactstrap and basic routing set

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+
+import EnterPhone from './Pages/EnterPhone/EnterPhone';
+import EnterOTP from './Pages/EnterOTP/EnterOTP';
+import Profile from './Pages/Profile/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/enterphone" component={EnterPhone} />
+        <Route exact path="/enterotp" component={EnterOTP} />
+        <Route exact path="/profile" component={Profile} />
+      </Switch>
+    </Router>
   );
 }
 
