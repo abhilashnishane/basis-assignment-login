f516361f31884c4ed58f6edd2353e95be24c8435
Abhilash Nishane <abhilash.nishane6@gmail.com>
2020-11-19 13:11:01 +0530

    basic EnterPhone page done with api call

--- src/Pages/EnterPhone/EnterPhone.js
+++ src/Pages/EnterPhone/EnterPhone.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Container, Row, Col } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
+const BASE_URL = 'https://hiring.getbasis.co/candidate';
 
 export default function EnterPhone() {
+  const history = useHistory();
+  const [phone, setPhone] = useState('');
+
+  function phoneChange(e) {
+    const { name, value } = e.target;
+    setPhone(value);
+  }
+
+  function phoneSubmit(e) {
+    e.preventDefault();
+
+    if (phone) {
+
+      fetch(BASE_URL + '/users/phone', {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          phoneNumber: phone
+        })
+      })
+        .then((response) => response.json())
+        .then((json) => {
+
+          console.log(json);
+          if(json.success) {
+            console.log(json.results.token);
+            localStorage.setItem("basis_token", json.results.token);
+            history.push('/enterotp');
+          }
+
+        })
+        .catch((error) => console.error(error))
+
+    }
+  }
+
   return (
-    <div>
-      Enter phone number
-    </div>
+    <Container>
+      <div>Enter phone number</div>
+      <Form onSubmit={phoneSubmit}>
+        <FormGroup>
+          <Label for="">Phone</Label>
+          <Input type="number" name="phone" value={phone} onChange={phoneChange} required />
+        </FormGroup>
+        <Button color="primary">Next</Button>
+      </Form>
+    </Container>
   )
 }
